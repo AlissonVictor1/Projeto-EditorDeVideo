84282c78748b2633ee761cd7118b52f746b1c92d
unknown <suporte@SUPORTE2.batista.local>
2024-09-04 10:23:18 -0300

    Teste 01

--- src/main/java/org/example/editor/VideoClip.java
+++ src/main/java/org/example/editor/VideoClip.java
@@ -3,5 +3,5 @@ package org.example.editor;
 public class VideoClip {
     //Descrição: Representa um clipe de vídeo. Inclui informações sobre o caminho do arquivo e o intervalo de tempo que o clipe deve cobrir.
     //Métodos: Métodos para acessar e modificar o caminho do arquivo e os tempos de início e fim.
-    privaet Str
+    private String
 }
